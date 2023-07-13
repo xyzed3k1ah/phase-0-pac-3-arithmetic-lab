eded97a468c7f37945b0b191a6f1b7c141488f3a
Zedikiah <xyzed3k1ah@gmail.com>
2023-07-13 16:18:39 -0600

    Javascript Calculator Challenge Lab

---
+++ index.js
@@ -0,0 +1,37 @@
+function divide() {
+    return "something";
+  }
+
+  function add(a, b) {
+    return a + b;
+  }
+
+  function subtract(a, b) {
+    return a +- b;
+  }
+
+  function multiply(a, b) {
+    return a * b;
+  }
+
+  function divide(a, b) {
+    return a / b;
+  }
+
+  function increment(n) {
+    return ++n
+  }
+
+  function decrement(n) {
+    return --n
+  }
+
+  function makeInt(n) {
+    parseInt(n, 10)
+    return parseInt(n, 10)
+  }
+
+  function preserveDecimal(n) {
+    parseFloat (n)
+    return parseFloat (n)
+  }
